e8bfd75efca29accdd1dc0a171eea40aab9b2442
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2023-08-02 23:39:06 +0900

    関数の戻り値に型をつける

--- neko/pages/index.tsx
+++ neko/pages/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import { useState } from "react";
 
 /**
  * サバイバルナイフTypeScript 作って学ぶ「Next.jsで猫画像ジェネレーターを作ろう」
@@ -6,8 +7,18 @@ import { NextPage } from "next";
  * ・The Cat APIにリクエストし、猫の画像を取得する関数を実装
 */
 
+// const [count, setCount] = useState(100);
+// const onClickCountButton = () => {
+//   setCount(count + 1);
+// };
+
 const IndexPage: NextPage = () => {
-  return <div>ねこ</div>;
+  return (
+    <>
+      <div>ねこ</div>
+      {/* <button onClick={onClickCountButton}>{count}</button> */}
+    </>
+  );
 };
 // Next.jsでは、1ファイルにつき1ページコンポーネントを作成
 // NextPage: ページコンポーネントを表す型
@@ -15,11 +26,17 @@ const IndexPage: NextPage = () => {
 export default IndexPage;
 // Next.jsにページコンポーネントと認識させるためexport default
 
-const fetchCatsImage = async () => {
+type Image = {
+  url: string;
+}
+const fetchCatsImage = async (): Promise<Image> => {
   const response = await fetch("https://api.thecatapi.com/v1/images/search"); // Responseオブジェクトを返す
   const catsImages = await response.json(); // Responseオブジェクトのjson()メソッドを実行 -> レスポンスのボディーをJSONとしてパースし、jsのオブジェクトとして取得
   console.log(catsImages);
   return catsImages[0];
 }
+fetchCatsImage().then((catsImage) => {
+  // console.log(catsImage.alt); id, url, width, heightだけあるので、altは存在しない。catsImagesがany型だと、誤ったコードを書いてもエラーにならない。
+});
 
 // fetchCatsImage();
